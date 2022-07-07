5e0ad0faae87cce791f7902200a93d7a9328cf4c
Varun Sharma <varunsharma@sharechat.co>
2022-07-07 17:02:56 +0530

    Testing CHanges

--- cmd/tusd/main.go
+++ cmd/tusd/main.go
@@ -8,6 +8,7 @@ func main() {
 	cli.ParseFlags()
 	cli.PrepareGreeting()
 
+	//Testing Sharechat
 	// Print version and other information and exit if the -version flag has been
 	// passed else we will start the HTTP server
 	if cli.Flags.ShowVersion {
